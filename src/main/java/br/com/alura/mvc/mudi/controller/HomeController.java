b0490d6690a68f6b882e471417ae7a379d8c9dbc
6051 <franciscofalcao@tce.pe.gov.br>
2021-02-25 16:00:44 -0300

    Using JPA

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,6 +3,10 @@ package br.com.alura.mvc.mudi.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,18 +16,15 @@ import br.com.alura.mvc.mudi.model.Pedido;
 @Controller
 public class HomeController {
 
+	@PersistenceContext
+	private EntityManager entityManager;
+		
 	@GetMapping("/home")
 	public String home(Model model) {
 		
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Iphone 11 Apple Preto, 128gb");
-		pedido.setUrlProduto("pf_rd_m=A3RN7G7QC5MWSZ&pf_rd_p=da40754c-44c1-4214-8e5f-e03480961ec2&pf_rd_r=9ADTTJAR8RRD9JKN77Q3&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614257395&refinements=p_4%3AAPPLE%7CApple%7CiPhone%7CiPhone+7+4.7\"%7CiPhone+7+Plus&s=wireless&sr=1-20");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/41u6SlsJvLL._AC_.jpg");
-		pedido.setDescricao("Uma descrição qualquer para esse pedido.");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
 		model.addAttribute("pedidos", pedidos);
-		
 		return "home";
 	}
 	
