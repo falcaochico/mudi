b965c6c31a977445bb4d62d14a55c4b3de6544e1
6051 <franciscofalcao@tce.pe.gov.br>
2021-02-26 07:59:07 -0300

    Using ModelAndView method

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
@@ -17,11 +18,12 @@ public class HomeController {
 	private PedidoRepository pedidoRepository;
 	
 	@GetMapping("/home")
-	public String home(Model model) {
+	public ModelAndView home(Model model) {
 		
 		List<Pedido> pedidos = pedidoRepository.findAll();
 		model.addAttribute("pedidos", pedidos);
-		return "home";
+		ModelAndView mv = new ModelAndView("home");
+		return mv;
 	}
 	
 }
