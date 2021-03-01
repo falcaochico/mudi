0a49bf7b03f1e11ac59cf147c2efa069f2d32dc0
6051 <franciscofalcao@tce.pe.gov.br>
2021-03-01 08:03:38 -0300

    creating 'PedidoDTO and saving 'Produto'

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,15 +1,29 @@
 package br.com.alura.mvc.mudi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.dto.PedidoDTO;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
 
+	@Autowired
+	PedidoRepository pedidoRepository;
+	
 	@GetMapping("formulario")
 	public String formulario() {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(PedidoDTO pedidoDto) {
+		pedidoRepository.save(pedidoDto.toEntity());
 		return "pedido/formulario";
 	}
 }
